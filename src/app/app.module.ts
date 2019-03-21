4b7d094efd0716a441a651c89b6516913c05a471
@antonio25x <antonio17x@hotmail.com>
2019-03-20 22:43:49 -0400

    Declare all components in app module Import routing module from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ClientComponent } from './client/client.component';
+import { HomeComponent } from './home/home.component';
+import { CotizacionComponent } from './cotizacion/cotizacion.component';
+import { DocsComponent } from './docs/docs.component';
+import { FacturaComponent } from './factura/factura.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { VehiculoComponent } from './vehiculo/vehiculo.component';
+import { OrdenesComponent } from './ordenes/ordenes.component';
+import { HistorialComponent } from './historial/historial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    HomeComponent,
+    CotizacionComponent,
+    DocsComponent,
+    FacturaComponent,
+    InventarioComponent,
+    VehiculoComponent,
+    OrdenesComponent,
+    HistorialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
