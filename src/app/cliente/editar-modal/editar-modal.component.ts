d112a913d28176870f1cceee295a415ce4332627
antonio25x <antonio17x@hotmail.com>
2019-03-23 14:06:14 -0400

    Create editar modal component

---
+++ src/app/cliente/editar-modal/editar-modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Cliente } from '../cliente';
+
+@Component({
+  selector: 'app-editar-modal',
+  templateUrl: './editar-modal.component.html',
+  styleUrls: ['./editar-modal.component.css']
+})
+export class EditarModalComponent implements OnInit {
+
+  cliente: Cliente;
+
+  constructor(public activeModal: NgbActiveModal) {
+   }
+
+  ngOnInit() {
+  }
+
+  guardarCliente() {
+    this.activeModal.close(new Cliente(this.cliente.nombre, this.cliente.direccion));
+  }
+}
