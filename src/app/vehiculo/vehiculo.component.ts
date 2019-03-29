c2c61db992409353861676bdd32733423c8ff648
antonio25x <antonio17x@hotmail.com>
2019-03-28 20:36:17 -0400

    Update img url to fix missing images in vehiculo screen

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -14,8 +14,8 @@ export class VehiculoComponent implements OnInit {
 
     ngOnInit(): void {
         this.vehiculos = new Array<Vehiculo>();
-        this.vehiculos.push(new Vehiculo('Ford Focus', 2011, 10000, '../../assets/images/cars/2011_focus.png'));
-        this.vehiculos.push(new Vehiculo('Lexus', 2017, 1520, '../../assets/images/cars/2017_lexus.jpg'));
-        this.vehiculos.push(new Vehiculo('Genesis', 2019, 164200, '../../assets/images/cars/2019_genesis.jpg'));
+        this.vehiculos.push(new Vehiculo('Ford Focus', 2011, 10000, 'assets/images/cars/2011_focus.png'));
+        this.vehiculos.push(new Vehiculo('Lexus', 2017, 1520, 'assets/images/cars/2017_lexus.jpg'));
+        this.vehiculos.push(new Vehiculo('Genesis', 2019, 164200, 'assets/images/cars/2019_genesis.jpg'));
     }
 }
