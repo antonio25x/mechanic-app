ca25d0a0b1123c9bfd81eb2bbb43cc5e94904be8
antonio25x <antonio17x@hotmail.com>
2019-03-28 20:25:43 -0400

    Update vehiculo screen to display three cars
    Arrange the cars in three columns
    Use cards to display the cars
    Make the cards links

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from './vehiculo.model';
 
 @Component({
     selector: 'app-vehiculo',
     templateUrl: './vehiculo.component.html',
     styleUrls: ['./vehiculo.component.css']
 })
-export class VehiculoComponent {
+export class VehiculoComponent implements OnInit {
 
+    vehiculos: Array<Vehiculo>;
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.vehiculos = new Array<Vehiculo>();
+        this.vehiculos.push(new Vehiculo('Ford Focus', 2011, 10000, '../../assets/images/cars/2011_focus.png'));
+        this.vehiculos.push(new Vehiculo('Lexus', 2017, 1520, '../../assets/images/cars/2017_lexus.jpg'));
+        this.vehiculos.push(new Vehiculo('Genesis', 2019, 164200, '../../assets/images/cars/2019_genesis.jpg'));
+    }
 }
---
+++ src/app/vehiculo/vehiculo.model.ts
@@ -0,0 +1,8 @@
+
+export class Vehiculo {
+    constructor(
+        public nombre: string,
+        public ano: number,
+        public kilometros: number,
+        public imgUrl: string) { }
+}
